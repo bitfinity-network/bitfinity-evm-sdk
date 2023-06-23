80f424daeb93b76a3b9b13bb529bab64ce5604d7
f3 <f3kilo@yandex.ru>
2023-06-23 07:55:53 +0300

    typo fixed

--- src/eth-signer/src/ic_sign.rs
+++ src/eth-signer/src/ic_sign.rs
@@ -105,7 +105,7 @@ impl IcSigner {
         // The only thing we can do: try to recover address and, if failed,
         // assume that recovery id should be increased.
         let recovered = signature.recover(hash.0)?;
-        let tx_from = tx.from().ok_or(IcSignerError::FromAdderessNotPresent)?;
+        let tx_from = tx.from().ok_or(IcSignerError::FromAddressNotPresent)?;
         if &recovered != tx_from {
             signature.v += 1;
         };
