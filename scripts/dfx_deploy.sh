af41fe9ef58476cf3b70feeca2081d3c4b0f7d48
f3 <f3kilo@yandex.ru>
2023-07-03 12:06:46 +0300

    scripts restored

--- scripts/dfx_deploy.sh
+++ scripts/dfx_deploy.sh
@@ -7,8 +7,8 @@ dfx start --clean --background
 # Wait for dfx started
 sleep 5
 
-# echo "Deploying iceth-client artifacts..."
-# ./src/iceth-client/scripts/dfx_deploy.sh
+echo "Deploying iceth-client artifacts..."
+./src/iceth-client/scripts/dfx_deploy.sh
 
 echo "Deploying eth-signer artifacts..."
 dfx deploy ic-sign-test-canister
