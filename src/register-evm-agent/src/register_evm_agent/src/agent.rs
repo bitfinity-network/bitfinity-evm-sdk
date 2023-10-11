3e1fcf31aa7c4ba1afa3e653be1d994680b3ef45
Francesco_Cina <ufoscout@gmail.com>
2023-10-11 08:36:45 +0200

    remove duplicated code

--- src/register-evm-agent/src/register_evm_agent/src/agent.rs
+++ src/register-evm-agent/src/register_evm_agent/src/agent.rs
@@ -1,34 +1,7 @@
-use std::path::Path;
-
 use candid::Principal;
-use evm_canister_client::ic_agent::agent::http_transport::ReqwestHttpReplicaV2Transport;
 use evm_canister_client::ic_agent::Agent;
 
-mod generic_identity;
-use generic_identity::GenericIdentity;
-
 use crate::error::{Error, Result};
-
-/// Initialize an IC Agent
-pub async fn init_agent(identity_path: &Path, url: &str) -> Result<Agent> {
-    info!("parsing identity from {}", identity_path.display());
-    let identity = GenericIdentity::try_from(identity_path)?;
-    info!("identity parsed");
-
-    info!("network url: {url}");
-    let transport = ReqwestHttpReplicaV2Transport::create(url)?;
-
-    let agent = Agent::builder()
-        .with_transport(transport)
-        .with_identity(identity)
-        .build()?;
-
-    info!("agent built; fetching root key...");
-    agent.fetch_root_key().await?;
-    info!("agent initialized");
-
-    Ok(agent)
-}
 
 /// Returns `Principal` from ic agent
 pub fn user_principal(agent: &Agent) -> Result<Principal> {
