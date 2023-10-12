bbefb6a8a14c3383165ddece9d90887aa935ad5f
Francesco_Cina <ufoscout@gmail.com>
2023-10-12 17:28:22 +0200

    refactor test scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,37 @@
 #!/bin/bash
-
+set -e
+export RUST_BACKTRACE=full
 export WASM_DIR=./target/artifact
-mkdir -p $WASM_DIR
 
-echo "Building iceth-client artifacts..."
-./src/iceth-client/scripts/download_iceth.sh
-./src/iceth-client/scripts/build_test_canister.sh
+build_iceth_client_test_canisters() {
 
-echo "Building eth-signer artifacts..."
-./src/eth-signer/scripts/build_test_canister.sh
+    TAG=v0.1.1
+    ICETH_URL=https://github.com/infinity-swap/iceth/releases/download/${TAG}/iceth-${TAG}.tar.gz
+    echo "Downloading $ICETH_URL"
+    curl -fsSL $ICETH_URL | tar -xz -C "$WASM_DIR"
+
+    echo "Building iceth-client test canister"
+    cargo run -p iceth-client-test-canister --features export-api --release > $WASM_DIR/iceth-client-test-canister.did
+    cargo build -p iceth-client-test-canister --target wasm32-unknown-unknown --features export-api --release
+    ic-wasm target/wasm32-unknown-unknown/release/iceth-client-test-canister.wasm -o $WASM_DIR/iceth-client-test-canister.wasm shrink
+    gzip -k $WASM_DIR/iceth-client-test-canister.wasm --force
+
+}
+
+build_ic_sign_test_canister() {
+    echo "Building ic-sign-client test canisters"
+    cargo run -p ic-sign-test-canister --features "export-api" > $WASM_DIR/ic-sign-test-canister.did
+    cargo build -p ic-sign-test-canister --target wasm32-unknown-unknown --release --features "export-api"
+    ic-wasm target/wasm32-unknown-unknown/release/ic-sign-test-canister.wasm -o $WASM_DIR/ic-sign-test-canister.wasm shrink
+    gzip -k $WASM_DIR/ic-sign-test-canister.wasm --force
+}
+
+main() {
+    mkdir -p $WASM_DIR
+
+    build_iceth_client_test_canisters
+    build_ic_sign_test_canister
+
+}
+
+main "$@"
--- scripts/dfx_deploy.sh
+++ scripts/dfx_deploy.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
+set -e
 
 export WASM_DIR=./target/artifact
 
-dfx start --clean --background
+deploy_iceth_client_test_canisters() {
+    echo "Deploying iceth artifact..."
+    dfx deploy iceth 
+    
+    echo "Deploying iceth-client artifact..."
+    ICETH_PRINCIPAL=$(dfx canister id iceth)
+    INIT_ARGS="record { url=\"https://testnet.bitfinity.network\"; iceth=principal \"$ICETH_PRINCIPAL\"; chain_id=355113}"
+    dfx deploy iceth-client-test-canister --argument "$INIT_ARGS" -m reinstall -y
+}
 
-# Wait for dfx started
-sleep 5
+deploy_ic_sign_test_canister() {
+    echo "Deploying ic-sign-test-canister artifacts..."
+    dfx deploy ic-sign-test-canister
+}
 
-echo "Deploying iceth-client artifacts..."
-./src/iceth-client/scripts/dfx_deploy.sh
+main() {
 
-echo "Deploying eth-signer artifacts..."
-dfx deploy ic-sign-test-canister
+    dfx stop || killall dfx icx-proxy || true
 
+    dfx start --clean --background
+    # Wait for dfx started
+    sleep 5
 
+    deploy_iceth_client_test_canisters
+    deploy_ic_sign_test_canister
+}
+
+main "$@"
