225b9011dd2cb854b4ac48eafbca3fd99fe839b6
f3 <f3kilo@yandex.ru>
2023-11-10 09:05:11 +0300

    impl Indices

--- src/did/src/state.rs
+++ src/did/src/state.rs
@@ -1,9 +1,12 @@
+use std::borrow::Cow;
 use std::io::Cursor;
 use std::{fmt, mem};
 
 use candid::CandidType;
+use ic_stable_structures::{Bound, Storable};
 use serde::{Deserialize, Serialize};
 
+use crate::codec::ByteChunkReader;
 use crate::U256;
 
 /// Describes basic state of an EVM account.
@@ -29,6 +32,34 @@ pub struct Indices {
     pub pending_block: u64,
     /// Number of block to keep history
     pub history_size: u64,
+}
+
+impl Indices {
+    const STORABLE_BYTE_SIZE: usize = mem::size_of::<u64>() * 2;
+}
+
+impl Storable for Indices {
+    fn to_bytes(&self) -> std::borrow::Cow<[u8]> {
+        let mut buf = Vec::with_capacity(Self::STORABLE_BYTE_SIZE);
+        buf.extend_from_slice(&self.pending_block.to_be_bytes());
+        buf.extend_from_slice(&self.history_size.to_be_bytes());
+        buf.into()
+    }
+
+    fn from_bytes(bytes: Cow<'_, [u8]>) -> Self {
+        let mut reader = ByteChunkReader::new(&bytes);
+        let pending_block = u64::from_be_bytes(*reader.read_slice());
+        let history_size = u64::from_be_bytes(*reader.read_slice());
+        Self {
+            pending_block,
+            history_size,
+        }
+    }
+
+    const BOUND: Bound = Bound::Bounded {
+        max_size: Self::STORABLE_BYTE_SIZE as _,
+        is_fixed_size: true,
+    };
 }
 
 /// Full information about entry
