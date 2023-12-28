0671d674a50db9969513efcad3eaaa23357afa0a
Yasir <goodwonder5@gmail.com>
2023-12-28 11:05:17 +0530

    Fixes

--- src/evm-block-extractor/src/bq.rs
+++ src/evm-block-extractor/src/bq.rs
@@ -1,8 +1,7 @@
-use super::BlockChainDB;
 use ethers_core::types::{Block, Transaction};
-//use gcp_bigquery_client::model::entry::Entry;
+use gcp_bigquery_client::model::entry::Entry;
 use gcp_bigquery_client::model::query_request::QueryRequest;
-//use gcp_bigquery_client::model::row::Row;
+use gcp_bigquery_client::model::row::Row;
 use gcp_bigquery_client::model::table_data_insert_all_request::TableDataInsertAllRequest;
 use gcp_bigquery_client::Client;
 use serde::Serialize;
@@ -17,19 +16,20 @@ pub struct BlockRow {
 }
 
 /// A client for BigQuery that can be used to query and insert data
-pub struct BigQueryBlockChain {
+pub struct BQ {
     client: Client,
     project_id: String,
     dataset_id: String,
     table_id: String,
 }
 
-impl BigQueryBlockChain {
+impl BQ {
+    /// Creates a new client for BigQuery
     pub async fn new(dataset_id: &str, table_id: &str) -> anyhow::Result<Self> {
         let sa_key = std::env::var("GOOGLE_APPLICATION_CREDENTIALS")
             .map_err(|_| anyhow::anyhow!("GOOGLE_APPLICATION_CREDENTIALS not set"))?;
 
-        let client = Client::from_service_account_key_file(&sa_key).await?;
+        let client = Client::from_service_account_key_file("./key.json").await?;
 
         Ok(Self {
             client,
@@ -38,33 +38,9 @@ impl BigQueryBlockChain {
             table_id: table_id.to_string(),
         })
     }
-}
-
-#[async_trait::async_trait]
-impl BlockChainDB for BigQueryBlockChain {
-    /// Creates a new client for BigQuery
-
-    async fn get_block_by_number(&self, block_number: u64) -> anyhow::Result<Block<Transaction>> {
-        let query = format!(
-            "SELECT body FROM `{project_id}.{dataset_id}.{table_id}` WHERE id = {block_number}",
-            project_id = self.project_id,
-            dataset_id = self.dataset_id,
-            table_id = self.table_id,
-        );
-        let response = self
-            .client
-            .job()
-            .query(self.project_id.as_str(), QueryRequest::new(query))
-            .await?;
-
-        let json = response.get_json_value(0)?;
-
-        let block: Block<Transaction> = serde_json::from_value(json.unwrap())?;
-        Ok(block)
-    }
 
     /// Returns the number of the last block stored in the zip file
-    async fn get_last_block_number(&self) -> anyhow::Result<u64> {
+    pub async fn get_last_block_number(&mut self) -> anyhow::Result<i64> {
         let query = format!(
             "SELECT MAX(id) FROM `{project_id}.{dataset_id}.{table_id}`",
             project_id = self.project_id,
@@ -81,16 +57,20 @@ impl BlockChainDB for BigQueryBlockChain {
             .get_i64_by_name("id")?
             .ok_or(anyhow::anyhow!("No id column in response"))?;
 
-        Ok(rows as u64)
+        Ok(rows)
     }
 
-    async fn insert_block(&mut self, block: Block<Transaction>) -> anyhow::Result<()> {
+    pub async fn insert_block(
+        &mut self,
+        block_number: u64,
+        block: &Block<Transaction>,
+    ) -> anyhow::Result<()> {
         let mut insert_request = TableDataInsertAllRequest::new();
 
         insert_request.add_row(
             None,
             BlockRow {
-                id: block.number.unwrap().as_u64(),
+                id: block_number,
                 body: serde_json::to_value(block)?,
             },
         )?;
@@ -106,24 +86,5 @@ impl BlockChainDB for BigQueryBlockChain {
             .await?;
 
         Ok(())
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    //use crate::storage_clients::gcp_big_query::BigQueryBlockChain;
-    use std::path::Path;
-
-    #[tokio::test]
-    async fn test_load_big_query_block_chain() {
-        let sa_key_path = "../../GCP_SA.json";
-        assert!(
-            Path::new(sa_key_path).exists(),
-            "Service account key file does not exist"
-        );
-
-        let client = gcp_bigquery_client::Client::from_service_account_key_file(sa_key_path)
-            .await
-            .unwrap();
     }
 }
