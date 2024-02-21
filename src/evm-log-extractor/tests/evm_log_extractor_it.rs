8d0ea767be729e98e6ed7a503d12d28827b6c716
Francesco <ufoscout@gmail.com>
2024-02-21 08:54:57 +0100

    Add log-extractor integration tests

--- src/evm-log-extractor/tests/evm_log_extractor_it.rs
+++ src/evm-log-extractor/tests/evm_log_extractor_it.rs
@@ -11,7 +11,7 @@ use tokio::io::{AsyncBufReadExt, BufReader};
 
 #[derive(Clone)]
 struct MockCanisterClient{
-    logs: Vec<String>
+    max_logs: usize,
 }
 
 #[async_trait::async_trait]
@@ -30,17 +30,22 @@ impl CanisterClient for MockCanisterClient {
         R: DeserializeOwned + CandidType,
     {
         if method == "ic_logs" {
-            let (offset, limit): (usize, usize) = decode_args(&encode_args(args).unwrap()).unwrap();
+            let (limit, offset): (usize, usize) = decode_args(&encode_args(args).unwrap()).unwrap();
             
-            let logs = self.logs[offset..offset + limit].iter().enumerate().map(|(i, log)| Log {
-                offset: i,
-                log: log.clone(),
-            }).collect::<Vec<_>>();
+            
+            let last_log = self.max_logs.min(offset + limit);
+            let first_log = offset.min(last_log);
+            
+            let mut logs = vec![];
+            for i in first_log..last_log {
+                logs.push(Log { log: format!("{i}"), offset: i });
+            }
 
             let logs = serde_json::to_value(Ok::<Logs, ()>(Logs {
                 logs,
-                all_logs_count: self.logs.len(),
+                all_logs_count: self.max_logs,
             })).unwrap();
+            
             Ok(serde_json::from_value(logs).unwrap())
         } else {
             panic!("should never call query with method {}", method);
@@ -51,7 +56,9 @@ impl CanisterClient for MockCanisterClient {
 #[tokio::test]
 async fn test_extract_logs() {
     // Arrange
-    let mock_client = MockCanisterClient;
+    let max_logs_in_canister = 1500;
+    let max_logs_per_call = 1000;
+    let mock_client = MockCanisterClient{ max_logs: max_logs_in_canister };
     let evm_client = EvmCanisterClient::new(mock_client);
 
     let temp_dir = tempfile::tempdir().unwrap();
@@ -60,54 +67,79 @@ async fn test_extract_logs() {
 
     let logs_settings = LogsJobSettings {
         path: logs_path.to_str().unwrap().to_string(),
-        max_logs_per_call: 1000,
+        max_logs_per_call,
         start_from_offset: Default::default(),
     };
 
     // Act 1 - get all logs
-    run_logs_job(evm_client.clone(), logs_settings.clone())
-        .await
-        .unwrap();
+    let log_file = {
+        run_logs_job(evm_client.clone(), logs_settings.clone())
+            .await
+            .unwrap();
+    
+        // Assert the logs file is created
+        assert!(logs_path.exists());
+        let log_file = read_dir(logs_path)
+            .await
+            .unwrap()
+            .next_entry()
+            .await
+            .unwrap()
+            .unwrap();
+        assert!(log_file.file_name().to_str().unwrap().ends_with(".log"));
+    
+        // Assert there is at least one log in the file (there should be at least the one from `admin_ic_permissions_add`)
+        let previous_logs_from_file = file_to_vec(log_file.path()).await;
+        assert_eq!(max_logs_per_call, previous_logs_from_file.len());
+    
+        // Assert the next start offset is updated
+        {
+            let start_from_offset = *logs_settings.start_from_offset.lock().await;
+            assert_eq!(max_logs_per_call,  start_from_offset);
+        }
 
-    // Assert the logs file is created
-    assert!(logs_path.exists());
-    let log_file = read_dir(logs_path)
-        .await
-        .unwrap()
-        .next_entry()
-        .await
-        .unwrap()
-        .unwrap();
-    assert!(log_file.file_name().to_str().unwrap().ends_with(".log"));
-
-    // Assert there is at least one log in the file (there should be at least the one from `admin_ic_permissions_add`)
-    let previous_logs_from_file = file_to_vec(log_file.path()).await;
-    assert!(!previous_logs_from_file.is_empty());
-    assert!(previous_logs_from_file.len() < logs_settings.max_logs_per_call);
-
-    // Assert the next start offset is updated
-    let previous_start_from_offset = { *logs_settings.start_from_offset.lock().await };
+        log_file
+    };
 
     // Act 2 - create a new log and get all logs
-    run_logs_job(evm_client.clone(), logs_settings.clone())
-        .await
-        .unwrap();
-
-    // Assert the new logs are appended to the file
-    let logs_from_file = file_to_vec(log_file.path()).await;
-    assert!(!logs_from_file.is_empty());
-    assert!(logs_from_file.len() > previous_logs_from_file.len());
-
-    // Assert the next start offset is updated
     {
-        let start_from_offset = *logs_settings.start_from_offset.lock().await;
-        assert!(start_from_offset > previous_start_from_offset);
+        run_logs_job(evm_client.clone(), logs_settings.clone())
+            .await
+            .unwrap();
+    
+        // Assert the new logs are appended to the file
+        let logs_from_file = file_to_vec(log_file.path()).await;
+        assert_eq!(max_logs_in_canister, logs_from_file.len());
+    
+        // Assert the next start offset is updated
+        {
+            let start_from_offset = *logs_settings.start_from_offset.lock().await;
+            assert_eq!(max_logs_in_canister,  start_from_offset);
+        }
+    
+        // Assert the logs are not duplicated
+        let mut unique_logs = logs_from_file.clone();
+        unique_logs.dedup();
+        assert_eq!(logs_from_file, unique_logs);
     }
 
-    // Assert the logs are not duplicated
-    let mut unique_logs = logs_from_file.clone();
-    unique_logs.dedup();
-    assert_eq!(logs_from_file, unique_logs);
+        // Act 3 - there are no more logs in the canister
+        {
+            run_logs_job(evm_client.clone(), logs_settings.clone())
+                .await
+                .unwrap();
+        
+            // Assert the new logs are appended to the file
+            let logs_from_file = file_to_vec(log_file.path()).await;
+            assert_eq!(max_logs_in_canister, logs_from_file.len());
+        
+            // Assert the next start offset is updated
+            {
+                let start_from_offset = *logs_settings.start_from_offset.lock().await;
+                assert_eq!(max_logs_in_canister,  start_from_offset);
+            }
+
+        }
 
 }
 
