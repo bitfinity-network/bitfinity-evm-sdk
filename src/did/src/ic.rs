de5bcc2325ef44e607c701be55a469ef396f09da
Francesco <ufoscout@gmail.com>
2024-05-24 09:36:13 +0200

    Add size estimate

--- src/did/src/ic.rs
+++ src/did/src/ic.rs
@@ -31,6 +31,7 @@ use serde::{Deserialize, Serialize};
 
 use crate::{Bytes, H160, U256};
 
+/// Account full data
 #[derive(Debug, candid::CandidType, PartialEq, Eq, Clone, Serialize, Deserialize)]
 pub struct RawAccountInfo {
     /// Account nonce.
@@ -43,10 +44,46 @@ pub struct RawAccountInfo {
     pub storage: Vec<(U256, U256)>,
 }
 
-pub type AccountInfoMap = BTreeMap<H160, RawAccountInfo>;
+impl RawAccountInfo {
+
+    /// Estimate the byte size of the account info.
+    pub fn estimate_byte_size(&self) -> usize {
+        const NONCE_SIZE: usize = U256::BYTE_SIZE;
+        const BALANCE_SIZE: usize = U256::BYTE_SIZE;
+        let bytecode_size = self.bytecode.as_ref().map(|b| b.0.len()).unwrap_or(0);
+        let storage_size = U256::BYTE_SIZE * 2 * self.storage.len();
+
+        NONCE_SIZE + BALANCE_SIZE + bytecode_size + storage_size
+    }
+
+}
+
+/// A Map from account address to account info.
+#[derive(Debug, candid::CandidType, PartialEq, Eq, Clone, Serialize, Deserialize)]
+pub struct AccountInfoMap{
+    pub data: BTreeMap<H160, RawAccountInfo>,
+}
+
+impl AccountInfoMap {
+
+    /// Estimate the byte size of the account info map.
+    pub fn estimate_byte_size(&self) -> usize {
+
+        const KEY_SIZE: usize = H160::BYTE_SIZE;
+        let mut total_size = KEY_SIZE * self.data.len();
+    
+        for (_address, account) in &self.data {
+            total_size += account.estimate_byte_size();
+        }
+    
+        total_size
+    }
+}
 
 #[cfg(test)]
 mod tests {
+
+    use candid::{Decode, Encode};
 
     use super::*;
 
@@ -65,5 +102,61 @@ mod tests {
             let decoded = StorablePrincipal::from_bytes(bytes);
             assert_eq!(source, decoded);
         }
+    }
+
+    #[test]
+    fn test_candid_encoding_raw_account() {
+        let account_info = RawAccountInfo {
+            nonce: U256::from(1u64),
+            balance: U256::from(2u64),
+            bytecode: Some(Bytes::from(vec![1, 2, 3])),
+            storage: vec![(U256::from(1u64), U256::from(2u64)), (U256::from(3u64), U256::from(4u64))],
+        };
+
+        let bytes = Encode!(&account_info).unwrap();
+        let decoded = Decode!(bytes.as_slice(), RawAccountInfo).unwrap();
+        assert_eq!(account_info, decoded);
+    }
+
+    #[test]
+    fn test_account_info_map_roundtrip() {
+        let account_info_map = AccountInfoMap {
+            data: [
+                (H160::from([1; 20]), RawAccountInfo {
+                    nonce: U256::from(1u64),
+                    balance: U256::from(2u64),
+                    bytecode: Some(Bytes::from(vec![1, 2, 3])),
+                    storage: vec![(U256::from(1u64), U256::from(2u64)), (U256::from(3u64), U256::from(4u64))],
+                }),
+            ].into(),
+        };
+
+        let bytes = Encode!(&account_info_map).unwrap();
+        let decoded = Decode!(bytes.as_slice(), AccountInfoMap).unwrap();
+        assert_eq!(account_info_map, decoded);
+    }
+
+    #[test]
+    fn test_estimate_byte_size() {
+        let account_info = RawAccountInfo {
+            nonce: U256::from(1u64),
+            balance: U256::from(2u64),
+            bytecode: Some(Bytes::from(vec![1, 2, 3])),
+            storage: vec![(U256::from(1u64), U256::from(2u64)), (U256::from(3u64), U256::from(4u64))],
+        };
+
+        let account_info_map = AccountInfoMap {
+            data: [
+                (H160::from([1; 20]), account_info.clone()),
+                (H160::from([2; 20]), account_info.clone()),
+                (H160::from([3; 20]), account_info.clone()),
+            ].into(),
+        };
+
+        let account_info_size = account_info.estimate_byte_size();
+        let account_info_map_size = account_info_map.estimate_byte_size();
+
+        assert_eq!(account_info_size, 32 + 32 + 3 + (32 * 4));
+        assert_eq!(account_info_map_size, 3 * (account_info_size + 20));
     }
 }
