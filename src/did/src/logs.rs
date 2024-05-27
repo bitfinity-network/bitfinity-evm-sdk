f86c4234375dbf6dfe3c8d3b600f904047a7a363
Yasir <goodwonder5@gmail.com>
2024-05-27 11:19:26 +0300

    chore: derive CandidType for Log

--- src/did/src/logs.rs
+++ src/did/src/logs.rs
@@ -1,3 +1,4 @@
+use candid::CandidType;
 use ethers_core::types::Bytes;
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
@@ -91,6 +92,99 @@ pub struct TransactionLog {
     pub data: Bytes,
     /// Log's Topics.
     pub topics: Vec<H256>,
+}
+
+/// A log produced by a transaction.
+#[derive(Debug, Clone, Default, PartialEq, Eq, CandidType, Serialize, Deserialize)]
+pub struct Log {
+    /// H160. the contract that emitted the log
+    pub address: H160,
+
+    /// topics: Array of 0 to 4 32 Bytes of indexed log arguments.
+    /// (In solidity: The first topic is the hash of the signature of the event
+    /// (e.g. `Deposit(address,bytes32,uint256)`), except you declared the event
+    /// with the anonymous specifier.)
+    pub topics: Vec<H256>,
+
+    /// Data
+    pub data: Vec<u8>,
+
+    /// Block Hash
+    #[serde(rename = "blockHash")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub block_hash: Option<H256>,
+
+    /// Block Number
+    #[serde(rename = "blockNumber")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub block_number: Option<U64>,
+
+    /// Transaction Hash
+    #[serde(rename = "transactionHash")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub transaction_hash: Option<H256>,
+
+    /// Transaction Index
+    #[serde(rename = "transactionIndex")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub transaction_index: Option<U64>,
+
+    /// Integer of the log index position in the block. None if it's a pending log.
+    #[serde(rename = "logIndex")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub log_index: Option<U256>,
+
+    /// Integer of the transactions index position log was created from.
+    /// None when it's a pending log.
+    #[serde(rename = "transactionLogIndex")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub transaction_log_index: Option<U256>,
+
+    /// Log Type
+    #[serde(rename = "logType")]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub log_type: Option<String>,
+
+    /// True when the log was removed, due to a chain reorganization.
+    /// false if it's a valid log.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub removed: Option<bool>,
+}
+
+impl From<ethers_core::types::Log> for Log {
+    fn from(log: ethers_core::types::Log) -> Self {
+        Self {
+            address: log.address.into(),
+            topics: log.topics.into_iter().map(Into::into).collect(),
+            data: log.data.0.into(),
+            block_hash: log.block_hash.map(Into::into),
+            block_number: log.block_number.map(Into::into),
+            transaction_hash: log.transaction_hash.map(Into::into),
+            transaction_index: log.transaction_index.map(Into::into),
+            log_index: log.log_index.map(Into::into),
+            transaction_log_index: log.transaction_log_index.map(Into::into),
+            log_type: log.log_type,
+            removed: log.removed,
+        }
+    }
+}
+
+impl From<Log> for ethers_core::types::Log {
+    fn from(log: Log) -> Self {
+        Self {
+            address: log.address.into(),
+            topics: log.topics.into_iter().map(Into::into).collect(),
+            data: log.data.into(),
+            block_hash: log.block_hash.map(Into::into),
+            block_number: log.block_number.map(Into::into),
+            transaction_hash: log.transaction_hash.map(Into::into),
+            transaction_index: log.transaction_index.map(Into::into),
+            log_index: log.log_index.map(Into::into),
+            transaction_log_index: log.transaction_log_index.map(Into::into),
+            log_type: log.log_type,
+            removed: log.removed,
+        }
+    }
 }
 
 #[cfg(test)]
