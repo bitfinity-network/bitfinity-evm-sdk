613df8a55e1b8ca2f201a0f1e72c0f4dae2cc597
Francesco <ufoscout@gmail.com>
2024-05-29 15:30:24 +0200

    Add StorableValue encode

--- src/did/src/state.rs
+++ src/did/src/state.rs
@@ -4,7 +4,7 @@ use candid::CandidType;
 use ic_stable_structures::{Bound, Storable};
 use serde::{Deserialize, Serialize};
 
-use crate::codec::ByteChunkReader;
+use crate::codec::{self, ByteChunkReader};
 use crate::U256;
 
 /// Describes basic state of an EVM account.
@@ -54,11 +54,85 @@ impl Storable for Indices {
 }
 
 /// Full information about entry
-#[derive(Clone, Debug, CandidType, Deserialize)]
-pub struct FullStorageValue {
+#[derive(Clone, Debug, CandidType, Deserialize, Serialize, PartialEq, Eq)]
+pub struct StorageValue {
     /// Data
     pub data: Vec<u8>,
     /// Number of inserts subtracted by number of removals.
     /// May be zero for the values which were removed in past before the moment they are cleaned.
     pub rc: u32,
+}
+
+impl Storable for StorageValue {
+    const BOUND: Bound = Bound::Unbounded;
+
+    fn to_bytes(&self) -> std::borrow::Cow<[u8]> {
+        codec::bincode_encode(self).into()
+    }
+
+    fn from_bytes(bytes: Cow<[u8]>) -> Self {
+        codec::bincode_decode(&bytes)
+    }
+}
+
+#[cfg(test)]
+mod test {
+
+    use candid::{Decode, Encode};
+
+    use super::*;
+
+    #[test]
+    fn test_candid_basic_account() {
+        let account = BasicAccount {
+            balance: U256::from(1u64),
+            nonce: U256::from(2u64),
+        };
+
+        let serialized = Encode!(&account).unwrap();
+        let deserialized = Decode!(serialized.as_slice(), BasicAccount).unwrap();
+
+        assert_eq!(account, deserialized);
+    }
+
+    #[test]
+    fn test_storable_indices() {
+        let indices = Indices {
+            pending_block: 1,
+            history_size: 2,
+        };
+
+        let serialized = indices.to_bytes();
+        let deserialized = Indices::from_bytes(serialized);
+
+        assert_eq!(indices, deserialized);
+    }
+
+    #[test]
+    fn test_storable_storage_value() {
+        let storage_value = StorageValue {
+            data: vec![1, 2, 3],
+            rc: 4,
+        };
+
+        let serialized = storage_value.to_bytes();
+        let deserialized = StorageValue::from_bytes(serialized);
+
+        assert_eq!(storage_value, deserialized);
+    }
+
+    #[test]
+    fn test_candid_storage_value() {
+        let storage_value = StorageValue {
+            data: vec![1, 2, 3],
+            rc: 4,
+        };
+
+        let serialized = Encode!(&storage_value).unwrap();
+        let deserialized = Decode!(serialized.as_slice(), StorageValue).unwrap();
+
+        assert_eq!(storage_value, deserialized);
+    }
+
+
 }
