7e665761ef2545e02cd11280615941661ea65df8
Yasir <goodwonder5@gmail.com>
2024-06-05 19:26:28 +0300

    EPROD-902 Add signature account

--- src/minter-did/src/reason.rs
+++ src/minter-did/src/reason.rs
@@ -1,8 +1,9 @@
 use candid::{CandidType, Principal};
 use did::keccak::keccak_hash;
 use did::transaction::Signature;
-use did::{H160, U256};
+use did::{H160, H256, U256};
 use ic_exports::icrc_types::icrc1::account::Subaccount;
+use ic_stable_structures::Storable;
 use serde::Deserialize;
 
 /// Information to perform burn operation for ICRC-2 token and create a mint order.
@@ -38,17 +39,61 @@ pub struct ApproveMintedTokens {
     /// Approve minted tokens amount.
     pub approve_amount: U256,
 
-    /// Signed `keccak_hash(caller's principal)` by the recipient wallet.
-    /// Reqired to prove caller's ownership of the wallet from which the minted tokens will be approved.
-    pub principal_signature: Signature,
+    /// Signed keccak256 hash of [ApproveMintSignature].
+    /// Required to prove caller's ownership of the wallet from which the minted tokens will be approved.
+    pub signature: Signature,
 }
 
-impl ApproveMintedTokens {
-    /// Checks if `self.principal_signature` is correct for the given `principal` and `signer`.
-    pub fn check_signature(&self, principal: &Principal, signer: &H160) -> Option<()> {
-        let eth_signature: ethers_core::types::Signature = self.principal_signature.clone().into();
-        let principal_hash = keccak_hash(principal.as_slice());
-        let recovered_signer = eth_signature.recover(principal_hash.0).ok()?;
+/// Represents the signature data required to approve minted tokens.
+///
+pub struct ApproveMintSignature {
+    pub approve_spender: H160,
+    pub approve_amount: U256,
+    pub chain_id: u32,
+    /// Expiration time in seconds since Unix epoch.
+    pub expiration: u64,
+    /// Nonce of the sender
+    pub nonce: u32,
+    pub token_principal: Principal,
+}
+
+impl ApproveMintSignature {
+    /// Creates a new signature
+    pub fn new(
+        approve_spender: H160,
+        approve_amount: U256,
+        nonce: u32,
+        expiration: u64,
+        token_principal: Principal,
+        chain_id: u32,
+    ) -> Self {
+        Self {
+            approve_spender,
+            approve_amount,
+            nonce,
+            expiration,
+            token_principal,
+            chain_id,
+        }
+    }
+
+    /// Hash of the signature data to be signed.
+    pub fn hash(&self) -> H256 {
+        let mut data = Vec::new();
+        data.extend_from_slice(&self.approve_spender.to_bytes());
+        data.extend_from_slice(&self.approve_amount.to_little_endian());
+        data.extend_from_slice(&self.chain_id.to_le_bytes());
+        data.extend_from_slice(&self.expiration.to_le_bytes());
+        data.extend_from_slice(&self.nonce.to_le_bytes());
+        data.extend_from_slice(self.token_principal.as_slice());
+
+        keccak_hash(data.as_slice())
+    }
+
+    pub fn check_signature(&self, signer: &H160, signature: Signature) -> Option<()> {
+        let eth_signature: ethers_core::types::Signature = signature.clone().into();
+        let hash = self.hash();
+        let recovered_signer = eth_signature.recover(hash.0).ok()?;
         if recovered_signer != signer.0 {
             return None;
         }
@@ -64,44 +109,33 @@ mod tests {
     use did::{H160, U256};
     use eth_signer::{Signer, Wallet};
 
+    use super::ApproveMintSignature;
     use super::ApproveMintedTokens;
-
     #[test]
     fn should_accept_correct_signature() {
         let principal = Principal::from_slice(&[42; 20]);
-        let principal_hash = keccak_hash(principal.as_slice());
+        let approve_signature_data =
+            ApproveMintSignature::new(H160::default(), U256::default(), 0, 0, principal, 0);
 
         let wallet = Wallet::new(&mut rand::thread_rng());
-        let signature = wallet.sign_hash(principal_hash.0).unwrap();
+        let signature = wallet.sign_hash(approve_signature_data.hash().0).unwrap();
 
-        let approve = ApproveMintedTokens {
-            approve_spender: H160::default(),
-            approve_amount: U256::default(),
-            principal_signature: signature.into(),
-        };
-
-        assert!(approve
-            .check_signature(&principal, &wallet.address().into())
+        assert!(approve_signature_data
+            .check_signature(&wallet.address().into(), signature.into())
             .is_some());
     }
 
     #[test]
     fn should_reject_invalid_signature() {
         let principal = Principal::from_slice(&[42; 20]);
-        let principal_hash = keccak_hash(principal.as_slice());
-
+        let mut approve_signature_data =
+            ApproveMintSignature::new(H160::default(), U256::default(), 0, 0, principal, 0);
         let wallet = Wallet::new(&mut rand::thread_rng());
-        let signature = wallet.sign_hash(principal_hash.0).unwrap();
+        let signature = wallet.sign_hash(approve_signature_data.hash().0).unwrap();
 
-        let approve = ApproveMintedTokens {
-            approve_spender: H160::default(),
-            approve_amount: U256::default(),
-            principal_signature: signature.into(),
-        };
-
-        let other_principal = Principal::from_slice(&[122; 20]);
-        assert!(approve
-            .check_signature(&other_principal, &wallet.address().into())
+        approve_signature_data.token_principal = Principal::from_slice(&[43; 20]);
+        assert!(approve_signature_data
+            .check_signature(&wallet.address().into(), signature.into())
             .is_none());
     }
 }
