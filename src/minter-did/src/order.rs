d697732bb9e2aaea92a70513ff3b326a97fcaf80
veeso <christian.visintin@veeso.dev>
2024-07-10 11:57:36 +0200

    EPROD-865 optional fields in mint order

--- src/minter-did/src/order.rs
+++ src/minter-did/src/order.rs
@@ -52,13 +52,13 @@ pub struct MintOrder {
     pub decimals: u8,
 
     /// Mint operation should approve tokens, using this address as a spender.
-    pub approve_spender: H160,
+    pub approve_spender: Option<H160>,
 
     /// Mint operation should approve this amount of tokens.
-    pub approve_amount: U256,
+    pub approve_amount: Option<U256>,
 
     /// Address of wallet from which fee will be charged.
-    pub fee_payer: H160,
+    pub fee_payer: Option<H160>,
 }
 
 impl MintOrder {
@@ -106,9 +106,27 @@ impl MintOrder {
         buf[148..180].copy_from_slice(&self.name);
         buf[180..196].copy_from_slice(&self.symbol);
         buf[196] = self.decimals;
-        buf[197..217].copy_from_slice(self.approve_spender.0.as_bytes());
-        buf[217..249].copy_from_slice(&self.approve_amount.to_big_endian());
-        buf[249..269].copy_from_slice(self.fee_payer.0.as_bytes());
+        buf[197..217].copy_from_slice(
+            self.approve_spender
+                .as_ref()
+                .unwrap_or(&H160::zero())
+                .0
+                .as_bytes(),
+        );
+        buf[217..249].copy_from_slice(
+            &self
+                .approve_amount
+                .as_ref()
+                .unwrap_or(&U256::zero())
+                .to_big_endian(),
+        );
+        buf[249..269].copy_from_slice(
+            self.fee_payer
+                .as_ref()
+                .unwrap_or(&H160::zero())
+                .0
+                .as_bytes(),
+        );
 
         let digest = keccak256(&buf[..Self::ENCODED_DATA_SIZE]);
 
@@ -142,9 +160,9 @@ impl MintOrder {
         let name = data[148..180].try_into().unwrap(); // exactly 32 bytes, as expected
         let symbol = data[180..196].try_into().unwrap(); // exactly 16 bytes, as expected
         let decimals = data[196];
-        let approve_spender = H160::from_slice(&data[197..217]);
-        let approve_amount = U256::from_big_endian(&data[217..249]);
-        let fee_payer = H160::from_slice(&data[249..269]);
+        let approve_spender = Self::decode_opt(&data[197..217], H160::from_slice);
+        let approve_amount = Self::decode_opt(&data[217..249], U256::from_big_endian);
+        let fee_payer = Self::decode_opt(&data[249..269], H160::from_slice);
 
         Some(Self {
             amount,
@@ -177,6 +195,18 @@ impl MintOrder {
                 .into();
 
         Some((decoded_data, signature))
+    }
+
+    /// Decode a slice of bytes as `T` if not zeroed.
+    fn decode_opt<T, F>(slice: &[u8], decode: F) -> Option<T>
+    where
+        F: FnOnce(&[u8]) -> T,
+    {
+        if slice.iter().any(|&b| b != 0) {
+            Some(decode(slice))
+        } else {
+            None
+        }
     }
 }
 
@@ -273,5 +303,64 @@ impl Storable for SignedMintOrder {
         Self(<[u8; MintOrder::SIGNED_ENCODED_DATA_SIZE]>::from_bytes(
             bytes,
         ))
+    }
+}
+
+#[cfg(test)]
+mod test {
+
+    use super::*;
+
+    #[test]
+    fn test_should_decode_optional() {
+        let data = [0u8; 20];
+        assert_eq!(MintOrder::decode_opt(&data, H160::from_slice), None);
+
+        let data = [1u8; 20];
+        assert_eq!(
+            MintOrder::decode_opt(&data, H160::from_slice),
+            Some(H160::from_slice(&data))
+        );
+    }
+
+    #[test]
+    fn test_should_decode_mint_order() {
+        let order = hex::decode("00000000000000000000000000000000000000000000000000000000000003e8000301020300000000000000000000000000000000000000000000000000000000040102030400000000000000000000000000000000000000000000000000002b5ad5c4795c026514f8317c7a215e218dccd6cf5615deb798bb3e4dfa0139dfa1b3d433cc23b72f000000000000000000007b43546f6b656e000000000000000000000000000000000000000000000000000000546f6b656e0000000000000000000000120000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000004e7760d3ad25ac8b3891b7bb837d842c0d97b8b625a79e50733d9ad984730b580e53cc7b20c052b6a0716f4521bd92383c45c555e9730b52fe09db9157ab37141b").unwrap();
+
+        let order = MintOrder::decode_data(&order).unwrap();
+        assert_eq!(order.amount, U256::from(1000u64));
+        assert_eq!(
+            order.sender,
+            Id256::from_slice(&[
+                0, 3, 1, 2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0
+            ])
+            .unwrap()
+        );
+        assert_eq!(
+            order.src_token,
+            Id256::from_slice(&[
+                0, 4, 1, 2, 3, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                0, 0, 0, 0
+            ])
+            .unwrap()
+        );
+        assert_eq!(
+            order.recipient,
+            H160::from_slice(&hex::decode("2b5ad5c4795c026514f8317c7a215e218dccd6cf").unwrap())
+        );
+        assert_eq!(
+            order.dst_token,
+            H160::from_slice(&hex::decode("5615deb798bb3e4dfa0139dfa1b3d433cc23b72f").unwrap())
+        );
+        assert_eq!(order.nonce, 0);
+        assert_eq!(order.sender_chain_id, 0);
+        assert_eq!(order.recipient_chain_id, 31555);
+        assert_eq!(order.name, fit_str_to_array("Token"));
+        assert_eq!(order.symbol, fit_str_to_array("Token"));
+        assert_eq!(order.decimals, 18);
+        assert_eq!(order.approve_spender, None);
+        assert_eq!(order.approve_amount, None);
+        assert_eq!(order.fee_payer, None);
     }
 }
