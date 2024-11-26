086d03ba00c871e8ba143c50325c3207bca6e19d
veeso <christian.visintin@veeso.dev>
2024-11-26 10:26:26 +0100

    fix: naminhg

--- src/did/src/unconfirmed_blocks.rs
+++ src/did/src/unconfirmed_blocks.rs
@@ -5,10 +5,10 @@ use crate::H256;
 
 #[derive(CandidType, Serialize, Deserialize, Debug)]
 /// Arguments to `validate_unconfirmed_blocks` function.
-pub struct ValidateUnconfirmedBlocksArgs {
+pub struct ValidateUnconfirmedBlockArgs {
     pub block_number: u64,
     pub block_hash: H256,
-    pub state_root_hash: H256,
-    pub transaction_root_hash: H256,
-    pub receipts_root_hash: H256,
+    pub state_root: H256,
+    pub transactions_root: H256,
+    pub receipts_root: H256,
 }
