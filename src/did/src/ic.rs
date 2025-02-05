7b70aebb12e6180b794dbbafeb69f8661e1cf071
Francesco <ufoscout@gmail.com>
2025-02-05 14:49:50 +0100

    Add block number to the block confirmation data

--- src/did/src/ic.rs
+++ src/did/src/ic.rs
@@ -140,6 +140,8 @@ impl Storable for BlockConfirmationStrategy {
 /// Data required to confirm a block and mark it `safe`.
 #[derive(Debug, Clone, CandidType, Serialize, Deserialize, PartialEq, Eq)]
 pub struct BlockConfirmationData {
+    /// the block number
+    pub block_number: u64,
     /// Hash of the block
     pub hash: H256,
     /// State root of the block
