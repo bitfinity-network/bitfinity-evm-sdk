16d7316ea6bb30e5465b2adaa03b56cf163d4768
Yasir <yasir@shariff.dev>
2025-02-19 15:26:15 +0300

    feat: add documentation for pow_transaction function parameters

--- src/did/src/utils.rs
+++ src/did/src/utils.rs
@@ -1,5 +1,12 @@
 use crate::{Transaction, H160, U256};
 
+/// Creates a Proof of Work (PoW) transaction with specified parameters
+/// This transaction is ephemeral and is not meant to be used in a real
+/// blockchain.
+/// # Arguments
+///
+/// * `base_fee` - Optional base fee to be added to gas price. If None, defaults to 0
+///
 pub fn pow_transaction(base_fee: Option<U256>) -> Transaction {
     let base_fee = base_fee.unwrap_or_default();
     Transaction {
